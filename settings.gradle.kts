3d9c7ad69e95589e99d90fe032a853889af1b0d0
Imran Malic Settuba <46971368+i-walker@users.noreply.github.com>
2022-03-17 20:44:22 +0100

    reorg analysis out of plugins folder (#9)
    * reorg
    
    * reorg

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,31 +36,31 @@ project(":arrow-gradle-plugin-commons").projectDir = File("libs/gradle-plugin-co
 // Analysis
 
 include(":arrow-analysis-types")
-project(":arrow-analysis-types").projectDir = File("plugins/analysis/types")
+project(":arrow-analysis-types").projectDir = File("analysis/types")
 
 include(":arrow-analysis-common")
-project(":arrow-analysis-common").projectDir = File("plugins/analysis/common")
+project(":arrow-analysis-common").projectDir = File("analysis/common")
 
 include(":arrow-analysis-kotlin-plugin")
-project(":arrow-analysis-kotlin-plugin").projectDir = File("plugins/analysis/kotlin-plugin")
+project(":arrow-analysis-kotlin-plugin").projectDir = File("analysis/kotlin-plugin")
 
 include(":arrow-analysis-java-plugin")
-project(":arrow-analysis-java-plugin").projectDir = File("plugins/analysis/java-plugin")
+project(":arrow-analysis-java-plugin").projectDir = File("analysis/java-plugin")
 
 include(":arrow-analysis-kotlin-gradle-plugin")
-project(":arrow-analysis-kotlin-gradle-plugin").projectDir = File("plugins/analysis/kotlin-gradle-plugin")
+project(":arrow-analysis-kotlin-gradle-plugin").projectDir = File("analysis/kotlin-gradle-plugin")
 
 include(":arrow-analysis-java-gradle-plugin")
-project(":arrow-analysis-java-gradle-plugin").projectDir = File("plugins/analysis/java-gradle-plugin")
+project(":arrow-analysis-java-gradle-plugin").projectDir = File("analysis/java-gradle-plugin")
 
 include(":arrow-analysis-laws")
-project(":arrow-analysis-laws").projectDir = File("plugins/analysis/laws")
+project(":arrow-analysis-laws").projectDir = File("analysis/laws")
 
 include(":arrow-analysis-example")
-project(":arrow-analysis-example").projectDir = File("plugins/analysis/example")
+project(":arrow-analysis-example").projectDir = File("analysis/example")
 
 include(":arrow-analysis-java-example")
-project(":arrow-analysis-java-example").projectDir = File("plugins/analysis/java-example")
+project(":arrow-analysis-java-example").projectDir = File("analysis/java-example")
 
 //includeBuild("arrow-analysis-sample")
 //includeBuild("arrow-proofs-example")
